f089adaf507775e9fa553aaa17303fdbf20224ae
ysc101 <yogesh.chavhan@prmgroup.in>
2025-04-10 17:13:24 +0530

    ChromeOptions addttion

--- src/main/java/baseclasses/BaseClass.java
+++ src/main/java/baseclasses/BaseClass.java
@@ -3,6 +3,7 @@ package baseclasses;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Map;
+import java.util.UUID;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,12 +11,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import java.util.HashMap;
 
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BaseClass {
 
 public static Map<String, String> mapAANumber = new HashMap<String, String>();
 public static Map<String, String> pannumberMap = new HashMap<String, String>();
 	public static WebDriver driver;
 	public void initializeBrowser() throws IOException {
+		String uniqueDir = "/tmp/chrome-profile-" + UUID.randomUUID();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--user-data-dir=" + uniqueDir);
+		//String uniqueDir = "/tmp/chrome-profile-" + System.currentTimeMillis();
 		WebDriverManager.chromedriver().setup();
 		System.setProperty("webdriver.http.factory", "jdk-http-client");
 		driver = new ChromeDriver();
@@ -24,6 +32,8 @@ public static Map<String, String> pannumberMap = new HashMap<String, String>();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		}
 	}
+
+
 
 
 	 	 
